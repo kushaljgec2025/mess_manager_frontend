0a0ab5b9e8a8cceb0f4b3fd0b49be9dc8ff5ee1f
Rohan Gope <rg2512@it.jgec.ac.in>
2024-03-22 20:08:36 +0530

    Refactor App component and remove unused CSS styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+	return <div>App</div>;
 }
 
 export default App;
