0553ce50a6861fe47eb7ae36ee1903088d9e55a3
Rohan Gope <rg2512@it.jgec.ac.in>
2024-04-13 21:42:01 +0530

    Update dependencies in package-lock.json and fix errors in DashBoard.jsx and MessByID.jsx

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
 	server: {
@@ -11,8 +9,4 @@ export default defineConfig({
 		},
 	},
 	plugins: [react()],
-
 });
-
-
-
